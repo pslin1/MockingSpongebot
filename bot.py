62dd069d9ba9d861e31926270d4ebe8211943ae0
Pierre Lin <pslin@ualberta.ca>
2021-01-22 18:55:24 -0800

    able to respond to prompt as message listener, change to bot command after

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+#This was created using the following tutorial as a reference:
+#https://realpython.com/how-to-make-a-discord-bot-python/#how-to-make-a-discord-bot-in-python
+
 import os
 
 import discord
@@ -9,8 +12,20 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 #client = discord.Client()
 
 class myClient(discord.Client):
-    async def on_ready():
+    async def on_ready(self):
         print(f'{client.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        #Check if the message is from itself
+        if message.author == client.user:
+            return
+
+        #Make this case insensitive
+        if message.clean_content == "!mock":
+            response = message.reference
+            print(response)
+            #if response:
+                #await message.channel.send(response)
 
 client = myClient()
 client.run(TOKEN)
