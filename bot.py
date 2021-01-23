2aca0d4cf89db91a84281f67ec52a042ec5906c0
Pierre Lin <pslin@ualberta.ca>
2021-01-22 20:51:40 -0800

    Cleanup comments

--- bot.py
+++ bot.py
@@ -11,27 +11,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-#client = discord.Client()
-
-# class myClient(discord.Client):
-#     async def on_ready(self):
-#         print(f'{client.user} has connected to Discord!')
-#
-#     async def on_message(self, message):
-#         #Check if the message is from itself
-#         if message.author == client.user:
-#             return
-#
-#         #Make this case insensitive
-#         if message.clean_content == "!mock":
-#             response = message.reference
-#             print(response)
-#             #if response:
-#                 #await message.channel.send(response)
-#
-# client = myClient()
-# client.run(TOKEN)
-
 bot = commands.Bot(command_prefix='!')
 
 #Things to check:
@@ -47,8 +26,9 @@ async def mock(ctx):
 
     response = msg.clean_content
 
+    for letter in response:
+
     if response:
         await ctx.send(response)
-    #print (ctx.message.reference.message_id)
 
 bot.run(TOKEN)
