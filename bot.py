68d5d7ffed4431f75cef0a781642af2a942e409c
Pierre Lin <pslin@ualberta.ca>
2021-01-23 14:08:16 -0800

    Basic functionalist, no exception handling

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@
 #https://realpython.com/how-to-make-a-discord-bot-python/#how-to-make-a-discord-bot-in-python
 
 import os
-
+import random
+import math
 import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -26,7 +28,24 @@ async def mock(ctx):
 
     response = msg.clean_content
 
-    for letter in response:
+    #Insert elements from response into index_array
+    index_array = []
+    for i in range(0,len(response)-1):
+        index_array.append(i)
+
+    #The number of letters to capitalize in the word
+    cap_number = math.ceil(len(response)*0.55)
+
+    random.seed(datetime.now())
+    #indexes from response for letters that are to be capitalized
+    capital_index =random.sample(index_array, k=cap_number)
+
+    response_list = list(response.lower())
+    for index in capital_index:
+        #https://www.w3schools.com/python/ref_string_capitalize.asp
+        response_list[index] = response[index].upper()
+
+    response = ''.join(response_list)
 
     if response:
         await ctx.send(response)
