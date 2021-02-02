7e2d50c43be6ae0fcc27aa3c4be582946fb5639f
Pierre Lin <pslin@ualberta.ca>
2021-02-01 22:40:01 -0800

    Handles no reference message

--- bot.py
+++ bot.py
@@ -21,31 +21,35 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='mock')
 async def mock(ctx):
 
-    msgID =ctx.message.reference.message_id
-    #https://stackoverflow.com/questions/61851174/how-to-get-message-by-id-discord-py
-    #Credit: diggy (https://stackoverflow.com/users/7808223/diggy)
-    msg = await ctx.fetch_message(msgID)
+    try:
+        msgID = ctx.message.reference.message_id
+        #https://stackoverflow.com/questions/61851174/how-to-get-message-by-id-discord-py
+        #Credit: diggy (https://stackoverflow.com/users/7808223/diggy)
+        msg = await ctx.fetch_message(msgID)
 
-    response = msg.clean_content
+        response = msg.clean_content
 
-    #Insert elements from response into index_array
-    index_array = []
-    for i in range(0,len(response)-1):
-        index_array.append(i)
+        #Insert elements from response into index_array
+        index_array = []
+        for i in range(0,len(response)-1):
+            index_array.append(i)
 
-    #The number of letters to capitalize in the word
-    cap_number = math.ceil(len(response)*0.55)
+        #The number of letters to capitalize in the word
+        cap_number = math.ceil(len(response)*0.55)
 
-    random.seed(datetime.now())
-    #indexes from response for letters that are to be capitalized
-    capital_index =random.sample(index_array, k=cap_number)
+        random.seed(datetime.now())
+        #indexes from response for letters that are to be capitalized
+        capital_index =random.sample(index_array, k=cap_number)
 
-    response_list = list(response.lower())
-    for index in capital_index:
-        #https://www.w3schools.com/python/ref_string_capitalize.asp
-        response_list[index] = response[index].upper()
+        response_list = list(response.lower())
+        for index in capital_index:
+            #https://www.w3schools.com/python/ref_string_capitalize.asp
+            response_list[index] = response[index].upper()
 
-    response = ''.join(response_list)
+            response = ''.join(response_list)
+
+    except:
+        response = None
 
     if response:
         await ctx.send(response)
